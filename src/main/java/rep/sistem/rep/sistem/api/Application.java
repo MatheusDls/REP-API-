526fdd9c69b01c83541fef1d92ec25d70129921d
MatheusDLS <matheusls11@hotmail.com>
2024-08-12 23:25:15 -0300

    inicial

---
+++ src/main/java/rep/sistem/rep/sistem/api/Application.java
@@ -0,0 +1,13 @@
+package rep.sistem.rep.sistem.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
